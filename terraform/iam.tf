99a314d086b24a004f3d9bd0fd4634ce0eb4cf2d
saiteja463 <109094460+saiteja463@users.noreply.github.com>
2026-03-16 14:00:58 -0700

    Add IAM role and policy for Lambda function

---
+++ terraform/iam.tf
@@ -0,0 +1,48 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "snapshot-cleanup-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name = "snapshot-cleanup-lambda-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeSnapshots",
+          "ec2:DeleteSnapshot"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
