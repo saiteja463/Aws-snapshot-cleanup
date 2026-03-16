8d73651fc7c6505e47bb24158776d5001a850225
saiteja463 <109094460+saiteja463@users.noreply.github.com>
2026-03-16 13:57:47 -0700

    Add AWS region and availability zone variables

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "availability_zone" {
+  description = "Availability zone"
+  type        = string
+  default     = "us-east-1a"
+}
